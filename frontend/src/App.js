2c4a9a59627146f5bc0b43c92025b8120d8a2a5e
jamilahmed2 <ja0935991@gmail.com>
2023-03-17 00:44:47 +0500

    routes/components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,54 @@
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// <!-- ========== Components  ========== -->
+import { Register } from './components/Register';
+import { Profile } from './components/Profile';
+import { Username } from './components/Username';
+import { Password } from './components/Password';
+import { Reset } from './components/Reset';
+import { Recovery } from './components/Recovery';
+import { NotFound } from './components/NotFound';
+
+// <!-- ========== Root Routes  ========== -->
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Username></Username>
+  },
+  {
+    path: '/register',
+    element: <Register></Register>
+  },
+  {
+    path: '/password',
+    element: <Password></Password>
+  },
+  {
+    path: '/profile',
+    element: <Profile></Profile>
+  },
+  {
+    path: '/reset',
+    element: <Reset></Reset>
+  },
+  {
+    path: '/recovery',
+    element: <Recovery></Recovery>
+  },
+  {
+    path: '*',
+    element: <NotFound></NotFound>
+  },
+])
 
 function App() {
+  // <!-- ========== App Title  ========== -->
+  document.title = "Login App"
+  // <!-- ========== ---  ========== -->
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <main>
+      <RouterProvider router={router}></RouterProvider>
+    </main>
   );
 }
 
---
+++ frontend/src/components/Username.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Username = () => {
+  return (
+    <div>Username</div>
+  )
+}
