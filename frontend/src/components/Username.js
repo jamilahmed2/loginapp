142ecbdb6d1b55085e7d6f2013089aa57198abdc
jamilahmed2 <ja0935991@gmail.com>
2023-03-17 01:56:06 +0500

    login ui

--- frontend/src/components/Username.js
+++ frontend/src/components/Username.js
@@ -1,7 +1,38 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import avatar from '../assests/profile.png'
+import styles from '../styles/Username.module.css'
 
 export const Username = () => {
-  return (
-    <div>Username</div>
-  )
+    return (
+        <div className="container mx-auto">
+
+            <div className='flex  justify-center items-center h-screen'>
+                <div className={styles.glass}>
+
+                    <div className="title flex flex-col items-center">
+                        <h4 className='text-4xl font-bold'>Hello Again!</h4>
+                        <span className=' text-center text-gray-500'>
+                            Explore More by connecting with us.
+                        </span>
+                    </div>
+
+                    <form  className="py-1">
+                        <div className='profile flex justify-center py-4'>
+                            <img src={avatar} className={styles.profile_img} alt="avatar" />
+                        </div>
+
+                        <div className="textbox flex flex-col items-center gap-6">
+                            <input className={styles.textbox} type="text" placeholder='Username' />
+                            <button type='submit' className={styles.btn}>Lets go</button>
+                        </div>
+
+                        <div className="text-center py-4">
+                            <span className='text-grey-500'>Not a Member <Link className='text-red-500' to='/register'>Register Now</Link></span>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div >
+    )
 }
