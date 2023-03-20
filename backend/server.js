60be171a0daa40bfa92794d6bffaff92dbb06432
jamilahmed2 <ja0935991@gmail.com>
2023-03-21 00:50:39 +0500

    backend setup with db conn

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import connect from "./Database/conn.js";
+
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(cors());
+app.use(morgan('tiny'));
+app.disable('x-powered-by'); // less hacker know about our stack
+
+
+const PORT = 5000;
+
+// HTTP GET Request
+app.get('/', (req, res) => {
+    res.status(201).json("Home GET Request")
+});
+
+// start server only when have a valid connection
+connect().then(() => {
+    try {
+        // start server
+        app.listen(PORT, () => {
+            console.log(`Server Connected to ${PORT}`);
+        })
+    } catch (error) {
+        console.log("Cannot connect to the server")
+    }
+}).catch(error=>{
+    console.log("Invalid database connection")
+})
+
